578fce488a08d02ec6f0621dd2c12e1ef26fc5c6
Tashi Gyatso <Tashigyatso45@gmail.com>
2023-09-12 15:44:51 -0400

    updated objectsball lab

---
+++ src/00-objectball.js
@@ -0,0 +1,63 @@
+function gameObject(){
+    return {
+    "home": {"teamname":"brooklyn nets",
+            "colors":["black","white"],
+            "players":{"alan anderson": {
+                    "number":,
+                    "shoe":,
+                    "points":,
+                    "rebounds:",
+                    "assists":,
+                    "steals":,
+                    "blocks":,
+                    "slam dunks":,
+                    
+            },
+                    "reggie evans":{
+                        "number":,
+                        "shoe":,
+                        "points":,
+                        "rebounds:",
+                        "assists":,
+                        "steals":,
+                        "blocks":,
+                        "slam dunks":,
+                        
+                },
+                    "brook lopez":{
+                        "number":,
+                        "shoe":,
+                        "points":,
+                        "rebounds:",
+                        "assists":,
+                        "steals":,
+                        "blocks":,
+                        "slam dunks":,
+                        
+                },
+                    "mason plumlee"{
+                        "number":,
+                        "shoe":,
+                        "points":,
+                        "rebounds:",
+                        "assists":,
+                        "steals":,
+                        "blocks":,
+                        "slam dunks":,
+                        
+                },
+                    "jason terry"{
+                        "number":,
+                        "shoe":,
+                        "points":,
+                        "rebounds:",
+                        "assists":,
+                        "steals":,
+                        "blocks":,
+                        "slam dunks":,
+                        
+                },
+            },
+        }
+    }
+}
